8a579f0d687b61f3c0b16cfb9fb0f0066890a804
KADIMISETTI MURALIKRISHNA (Digital) <KA351925@wipro.com>
2018-10-30 10:45:33 +0530

    new project added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { React , Component } from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return <div>Hi!</div>;
+}
+ReactDOM.render(<App />,document.querySelector('.container'));
